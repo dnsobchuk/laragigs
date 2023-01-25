e6b256e01c703ebee073d2b4fe14e0ce03486836
dnsobchuk <dnsobchuk@yandex.ru>
2023-01-25 22:42:47 +0300

    make routes and view for listings

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// All listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all(),
+    ]);
+});
+
+// Single Listing
+Route::get('/listing/{id}', function ($id) {
+    return view('listing', [
+        'listing' => Listing::find($id),
+    ]);
 });
