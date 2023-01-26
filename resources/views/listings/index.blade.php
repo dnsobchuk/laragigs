c769691d1777be4f750a74fa608a6e9978a5a426
dnsobchuk <dnsobchuk@yandex.ru>
2023-01-27 00:08:45 +0300

    make layout component

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,22 +1,21 @@
-@extends('layout')
+<x-layout>
 
-@section('content')
+    @include('partials._hero')
 
-@include('partials._hero')
+    @include('partials._search')
+        
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-5 md:space-y-0 mx-4">
 
-@include('partials._search')
-    
-<div class="lg:grid lg:grid-cols-2 gap-4 space-y-5 md:space-y-0 mx-4">
+        @unless(count($listings) == 0)
 
-@unless(count($listings) == 0)
+        @foreach($listings as $listing)
+            <x-listing-card :listing="$listing" />
+        @endforeach
+            
+        @else
+            <p>No listings found</p>
+        @endunless
 
-@foreach($listings as $listing)
-    <x-listing-card :listing="$listing" />
-@endforeach
-    
-@else
-    <p>No listings found</p>
-@endunless
+    </div>
 
-</div>
-@endsection
+</x-layout>
