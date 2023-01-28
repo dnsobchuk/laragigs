a3e0ad022e5125869eb9ba04928bc052b56467e9
dnsobchuk <dnsobchuk@yandex.ru>
2023-01-28 16:04:12 +0300

    make tag filter

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
-use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
     // Show all Listings
     public function index() {
         return view('listings.index', [
-            'listings' => Listing::all(),
+            'listings' => Listing::latest()
+            ->filter(request(['tag', 'search']))
+            ->get(),
         ]);
     }
 
